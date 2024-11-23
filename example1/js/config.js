bdf9da2d51eaa4879f0420fd650ecbda5401b8fd
viproxo <34965568+viproxo@users.noreply.github.com>
2024-11-23 21:35:13 +0100

    Add files via upload

---
+++ example1/js/config.js
@@ -0,0 +1,2 @@
+const MY_API_KEY = '19858753eemsh148c33ac134d941p17a3f3jsn0bccecc1760b'
+export { MY_API_KEY };
---
+++ example1/js/main.js
@@ -0,0 +1,16 @@
+const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '19858753eemsh148c33ac134d941p17a3f3jsn0bccecc1760b',
+		'x-rapidapi-host': 'quotes15.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
